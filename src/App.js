eefac7aa96458f1158e38cad6c169a422e84620d
Lyall S <lyall.s@intersectiq.com>
2023-06-26 20:40:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
+          Hi From Lyall !!!
+          New update in code
+          i was changed !
+
         </p>
         <a
           className="App-link"
